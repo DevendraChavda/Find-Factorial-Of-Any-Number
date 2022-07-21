65dd73762eb9d4421925d8708fbb38da41b941ca
Gaurav <makwanagauravkumar123@gmail.com>
2022-07-21 12:38:02 +0530

    Add Comments

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// This function multiply two number num1 * num 2
 string multiplyTwoNumbers(string num1, string num2) {
    if (num1 == "0" || num2 == "0") {
       return "0";
